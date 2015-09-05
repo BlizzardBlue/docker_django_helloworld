0a93a4c529cceb19e93ac8153e9f6204d545fa65
BlizzardBlue <dbdb6151@gmail.com>
2015-09-05 19:42:24 +0900

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,13 @@
+## urls.py
+
+from django.conf.urls import patterns, include, url
+from hackathon.views import hello
+
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+        url(r'^$', hello)
+)
